bdf9d0fa5fc0a29c63107bc399679966362e0076
franklynot <148856869+franklynot@users.noreply.github.com>
2025-03-21 17:51:49 +0100

    Add buy and sell functions in the bonding curve

--- src/BondingCurve.sol
+++ src/BondingCurve.sol
@@ -1,8 +1,65 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
 
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
+import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import { SafeERC20 } from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol'; // Inherits Context
+
 import { IBondingCurve } from './interfaces/IBondingCurve.sol';
 
-contract BondingCurve is IBondingCurve {
-    constructor() { }
+contract BondingCurve is IBondingCurve, Ownable {
+    using SafeERC20 for IERC20;
+
+    IERC20 public trendToken;
+    IERC20 public reserveToken;
+
+    constructor() Ownable(_msgSender()) { }
+
+    function initialize(address trendToken_, address reserveToken_) external override onlyOwner {
+        trendToken = IERC20(trendToken_);
+        reserveToken = IERC20(reserveToken_);
+    }
+
+    /**
+     * ==================== Read-only functions ====================
+     */
+    function trendTokenBalance() public view returns (uint256) {
+        return trendToken.balanceOf(address(this));
+    }
+
+    /**
+     * @notice Returns the amount of reserve token that can be purchased for the given amount of trend token.
+     */
+    function getReserveAmount(uint256 trendTokenAmount) public view returns (uint256) {
+        // TODO: Implement
+        uint256 amount = trendTokenAmount;
+
+        return amount;
+    }
+
+    /**
+     * ===================== Mutating functions =====================
+     */
+    function buyTrendToken(uint256 amount, address receiver) external override returns (uint256) {
+        if (receiver != address(0)) revert BondingCurve_ReceiverCannotBeZero();
+
+        uint256 maxBuyAmount = trendTokenBalance();
+        if (amount > maxBuyAmount) {
+            amount = maxBuyAmount;
+        }
+
+        uint256 reserveAmount = getReserveAmount(amount);
+        reserveToken.safeTransferFrom(_msgSender(), address(this), reserveAmount);
+        trendToken.safeTransfer(receiver, amount);
+
+        return amount;
+    }
+
+    function sellTrendToken(uint256 amount, address receiver) external override returns (uint256) {
+        if (receiver != address(0)) revert BondingCurve_ReceiverCannotBeZero();
+
+        // TODO: Implement
+
+        return amount;
+    }
 }
--- src/interfaces/IBondingCurve.sol
+++ src/interfaces/IBondingCurve.sol
@@ -1,4 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
 
-interface IBondingCurve { }
+interface IBondingCurve {
+    error BondingCurve_ReceiverCannotBeZero();
+
+    /**
+     * @notice Initializes the contract. Can only be called by the owner.
+     */
+    function initialize(address trendToken, address reserveToken) external;
+
+    /**
+     * @notice Executes a transaction to buy trend token for reserve token.
+     */
+    function buyTrendToken(uint256 amount, address receiver) external returns (uint256);
+
+    /**
+     * @notice Executes a transaction to sell trend token for reserve token.
+     */
+    function sellTrendToken(uint256 amount, address receiver) external returns (uint256);
+}
